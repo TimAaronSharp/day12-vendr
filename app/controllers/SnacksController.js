a4a9e8bc46c79e5b90b5dc6964b11508908777c3
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-02-25 17:03:23 -0700

    Snacks draw to view from Appstate

--- app/controllers/SnacksController.js
+++ app/controllers/SnacksController.js
@@ -6,6 +6,17 @@ export class SnacksController {
   constructor() {
     console.log("Hey, the SnacksController works!");
     console.log(AppState.snacks);
+    this.drawSnacks()
+  }
 
+  drawSnacks() {
+    const snacks = AppState.snacks
+    let innerHTMLContent = ''
+    const snackElem = document.getElementById('snackDisplaySection')
+
+    snacks.forEach(snack => {
+      innerHTMLContent += snack.snackHTMLTemplate
+    })
+    snackElem.innerHTML = innerHTMLContent
   }
 }
--- app/models/Snack.js
+++ app/models/Snack.js
@@ -3,6 +3,22 @@ export class Snack {
     this.name = snackName
     this.price = snackPrice
     this.picture = snackPicture
-    this.snackDescription = snackDescription
+    this.description = snackDescription
+  }
+  get snackHTMLTemplate() {
+    return /*html*/`
+    <div class="col-md-3">
+      <div class="d-flex justify-content-center">
+        <div class="card">
+          <div class="card-body">
+            <img src="${this.picture}" alt="${this.description}">
+            <div class="card-title">
+              <span>${this.name}</span><span>${this.price}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
   }
 }
